746b5711a7a0cc67a85dc3484486c65fb6b8a459
reiminn <144314029+reiminn@users.noreply.github.com>
2026-06-18 15:37:07 +0530

    Create allaboutsets.py

---
+++ college/pythonstar/allaboutsets.py
@@ -0,0 +1,15 @@
+#Learning more about sets
+#how to create sets?
+st=set()
+st={1,2,3}
+st.add(8)
+st.update(['item5','item6','item7'])
+#remove, discard, pop
+#union!!! st3 = st1.union(st2) #st3 = st1 | st2
+#st1.intersection(st2)
+# or using this : st1 & st2
+#Subset: issubset()
+#Super set: issuperset
+#st2.difference(st1) # set() : st2 - st1
+#st2.symmetric_difference(st1)
+###is disjoint
